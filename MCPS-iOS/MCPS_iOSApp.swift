2a84b49d42f3a2824b6f76d3a5acab2fab005c88
one-color-low <gakki.uec15@gmail.com>
2023-11-19 18:29:20 +0900

    Initial Commit

---
+++ MCPS-iOS/MCPS_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCPS_iOSApp.swift
+//  MCPS-iOS
+//
+//  Created by Yuta on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MCPS_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
